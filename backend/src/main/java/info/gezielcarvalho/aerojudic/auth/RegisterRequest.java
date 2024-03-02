c4208293a43c21503f8a030c80de2ef248f672d7
Geziel Carvalho <geziel.natal@gmail.com>
2024-03-02 16:38:36 -0500

    Finalize authentication. #5

---
+++ backend/src/main/java/info/gezielcarvalho/aerojudic/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package info.gezielcarvalho.aerojudic.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
