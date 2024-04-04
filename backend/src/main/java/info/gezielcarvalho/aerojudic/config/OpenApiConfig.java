93e852d55aa12951ecf1cbae3e56ae3765672952
Geziel Carvalho <geziel.natal@gmail.com>
2024-04-03 23:00:11 -0400

    Add token authentication to swagger documentation #5

--- backend/src/main/java/info/gezielcarvalho/aerojudic/config/OpenApiConfig.java
+++ backend/src/main/java/info/gezielcarvalho/aerojudic/config/OpenApiConfig.java
@@ -1,24 +1,29 @@
 package info.gezielcarvalho.aerojudic.config;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition
 @Configuration
 public class OpenApiConfig {
 
-    private final Components components = new Components();
-
     @Bean
-    public OpenAPI baseOpenAPI(){
-        return new OpenAPI().components(components)
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
                 .info(new Info()
                         .title("AeroJudic OpenAPI Docs")
-                        .version("1.0.1").description("API Documentation"));
+                        .version("1.0.1")
+                        .description("API Documentation"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerToken", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerToken"));
     }
-
 }
+
+
