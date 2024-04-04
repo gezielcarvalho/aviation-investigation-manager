efb91047b8ebf3c65fddd8d932c5a17a1773afd9
Geziel Carvalho <geziel.natal@gmail.com>
2024-04-03 22:40:25 -0400

    Add endpoints to whitelist. #5

--- backend/src/main/java/info/gezielcarvalho/aerojudic/config/SecurityConfiguration.java
+++ backend/src/main/java/info/gezielcarvalho/aerojudic/config/SecurityConfiguration.java
@@ -21,7 +21,17 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
-    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**"};
+    private static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-resources",
+            "/swagger-resources/**",
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
