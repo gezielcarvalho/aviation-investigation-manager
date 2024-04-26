2ff896241ca3d5f344c89f67d7d8151a97af6aad
Geziel Carvalho <geziel.natal@gmail.com>
2024-04-25 22:34:12 -0400

    Update readme and whitelist #11

--- backend/src/main/java/info/gezielcarvalho/aerojudic/config/SecurityConfiguration.java
+++ backend/src/main/java/info/gezielcarvalho/aerojudic/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package info.gezielcarvalho.aerojudic.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
